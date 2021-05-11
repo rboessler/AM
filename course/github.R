f7920df531959e068361490b898c2cf8f7c69a46
Magdalene Silberberger <magdalene.silberberger@uni-wh.de>
2021-05-11 14:10:34 +0200

    upload missing data file (110521)

---
+++ course/github.R
@@ -0,0 +1,6 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, usethis)
+
+use_git_config(user.name = "Magdalene Silberberger", user.email = "magdalene.silberberger@uni-wh.de")
+
+# https://happygitwithr.com/rstudio-git-github.html
